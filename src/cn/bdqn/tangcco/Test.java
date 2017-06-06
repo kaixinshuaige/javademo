2fe453e5668d8a9db97f5716407766e2eb2dedc9
kaixinshuaige <1311556887@qq.com>
2017-06-06 19:07:17 +0800

    这个文件我改过了

--- src/cn/bdqn/tangcco/Test.java
+++ src/cn/bdqn/tangcco/Test.java
@@ -1,10 +1,10 @@
-package cn.bdqn.tangcco;
+package src.cn.bdqn.tangcco;
 
 /**
- * Created by 马保生 on 2017/6/6.
+ * Created by Administrator on 2017/6/6.
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("马保生");
+        System.out.println("代铮");
     }
 }
