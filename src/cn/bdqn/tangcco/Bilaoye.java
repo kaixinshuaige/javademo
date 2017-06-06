aa2e1c74a2aee62970edc761a0edb70bc736cf53
bitianyu <991173773@qq.com>
2017-06-06 18:43:19 +0800

    新建

---
+++ src/cn/bdqn/tangcco/Bilaoye.java
@@ -0,0 +1,10 @@
+package cn.bdqn.tangcco;
+
+/**
+ * Created by 99117 on 2017/6/6.
+ */
+public class Bilaoye {
+    public static void main(String[] args) {
+        System.out.println("毕姥爷");
+    }
+}
